36db99a3e1a0a7b4a784c6f458905f6ba2d9d351
Takeru Ohta <phjgt308@gmail.com>
2019-03-08 08:18:05 +0900

    Update stats.sh

--- docker/stats.sh
+++ docker/stats.sh
@@ -4,5 +4,5 @@ set -eux
 
 NAME=$1
 
-rm /tmp/*.stats
+rm -f /tmp/${NAME}.stats
 watch -p -n 5 'echo "=== `date` ===" >> /tmp/'${NAME}'.stats; docker stats --no-stream | tee -a /tmp/'${NAME}'.stats'
